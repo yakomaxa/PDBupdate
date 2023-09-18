25cd043ebce55d3cf74773968fcb83343813d32a
yakomaxa <47655565+yakomaxa@users.noreply.github.com>
2023-09-19 00:08:45 +0900

    UPDATE license term

--- .github/scripts/generate_summary.py
+++ .github/scripts/generate_summary.py
@@ -92,7 +92,7 @@ def generate_html_from_tsv(file_name):
 
     html_content += """
 </div>
-
+<div>© Protein Data Bank Japan (PDBj) licensed under CC-BY-4.0 International</div>
 </body>
 </html>
 """
