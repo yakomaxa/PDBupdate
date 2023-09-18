f7a576e2d5a0541bd4484442cd11f6da459d17a0
yakomaxa <47655565+yakomaxa@users.noreply.github.com>
2023-09-18 20:03:05 +0900

    sixth commitush

--- .github/scripts/update_timestamp.py
+++ .github/scripts/update_timestamp.py
@@ -1,11 +1,12 @@
 import datetime
-filename = "docs/index.md"
+filename = "template/index.md"
+filename2 = "docs/index.md"
 with open(filename, "r") as file:
     content = file.read()
     
     timestamp = datetime.datetime.utcnow().strftime('%Y-%m-%d %H:%M:%S UTC')
     content = content.replace('HOGEHOGE', timestamp)
     
-    with open(filename, "w") as file:
+    with open(filename2, "w") as file:
         file.write(content)
         
